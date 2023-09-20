2e324511230d44d7dab7546a832b148ae39f3445
joshuashea <joshshea44@gmail.com>
2023-09-20 15:19:39 -0700

    created models

---
+++ src/models/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+const Food = require('./food.js');
+const Clothes = require('./clothes.js');
+
+const SQL_CONNECTION_STRING =
+  process.env.SQL_CONNECTION_STRING || 'sqlite:memory';
+
+const sequelize = new Sequelize(SQL_CONNECTION_STRING);
+
+module.exports = {
+  sequelize,
+  FoodModel: Food(sequelize, DataTypes),
+  ClothesModel: Clothes(sequelize, DataTypes),
+};
